95a842c723d4d3ebbceb74bdc0d3b00e47bc798e
Ryan Ross <ryan@reross.com>
2022-05-17 13:12:53 -0500

    created AddTodo component

---
+++ src/components/AddToDo.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from "react";
+import { TodoContext } from "../context/todoContext";
+import { TodoContextType, ITodo } from "../@types/todo";
+
+const AddTodo = () => {
+  const { saveTodo } = useContext(TodoContext) as TodoContextType;
+  const [formData, setFormData] = useState<ITodo | {}>();
+  const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    });
+  };
+
+  const handleSave = (e: React.FormEvent, formData: ITodo | any) => {
+    e.preventDefault();
+    saveTodo(formData);
+  };
+
+  return (
+    <form className="Form" onSubmit={(e) => handleSave(e, formData)}>
+      <div>
+        <div>
+          <label htmlFor="name">Title</label>
+          <input onChange={handleForm} type="text" id="title" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <input onChange={handleForm} type="text" id="description" />
+        </div>
+      </div>
+      <button disabled={formData === undefined ? true : false}>
+        {" "}
+        Add Todo
+      </button>
+    </form>
+  );
+};
+
+export default AddTodo;
